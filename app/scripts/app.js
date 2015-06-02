97413135166f7b985a7f0a57cb8ad5663e340390
Addy Osmani <addyosmani@gmail.com>
2015-06-02 22:35:50 +0100

    Fixes #103 - Remove unneeded removal of `unresolved`

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,8 +27,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
-    document.querySelector('body').removeAttribute('unresolved');
-
     // Ensure the drawer is hidden on desktop/tablet
     var drawerPanel = document.querySelector('#paperDrawerPanel');
     drawerPanel.forceNarrow = true;
