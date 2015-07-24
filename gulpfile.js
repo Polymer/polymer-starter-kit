a386c5239c9473ef88415b0e5ce7519e75648565
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-07-24 19:40:40 +0200

    Gulpfile: add callback to `clean` task
    Ref https://github.com/google/web-starter-kit/commit/6b4df236544cba9f115c731db721c8e865d5c6ad (Closes #269)

--- gulpfile.js
+++ gulpfile.js
@@ -175,7 +175,9 @@ gulp.task('precache', function (callback) {
 });
 
 // Clean Output Directory
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+gulp.task('clean', function (cb) {
+  del(['.tmp', 'dist'], cb);
+});
 
 // Watch Files For Changes & Reload
 gulp.task('serve', ['styles', 'elements', 'images'], function () {
