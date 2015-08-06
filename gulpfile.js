2d92d8367908638012220edbc4bdd0ea112caa20
Jeff Posnick <jeffy@google.com>
2015-08-06 14:31:22 -0400

    Comment tweaks.

--- gulpfile.js
+++ gulpfile.js
@@ -163,6 +163,10 @@ gulp.task('vulcanize', function () {
 // Generate config data for the <sw-precache-cache> element.
 // This include a list of files that should be precached, as well as a (hopefully unique) cache
 // id that ensure that multiple PSK projects don't share the same Cache Storage.
+// This task does not run by default, but if you are interested in using service worker caching
+// in your project, please enable it within the 'default' task.
+// See https://github.com/PolymerElements/polymer-starter-kit#enable-service-worker-support
+// for more context.
 gulp.task('cache-config', function (callback) {
   var dir = 'dist';
   var config = {
@@ -251,7 +255,7 @@ gulp.task('serve:dist', ['default'], function () {
 
 // Build Production Files, the Default Task
 gulp.task('default', ['clean'], function (cb) {
-  // Uncomment 'cache-config' after 'vulcanize' if your are going to use service workers.
+  // Uncomment 'cache-config' after 'vulcanize' if you are going to use service workers.
   runSequence(
     ['copy', 'styles'],
     'elements',
