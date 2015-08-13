5bfaa4bfa7c7ec9d05e1dcd353c72ba8c0ab24ee
Sam Saccone <samccone@gmail.com>
2015-08-12 23:05:44 -0400

    :mag: Run jshint on html changes.
    Run jshint on *.html file changes.
    
    Since a lot of your javascript lives in html files with polymer this is
    a pretty decent thing to do. It also prevents the flood of jshint messages whenyou all of a sudden change a .js file :warning:

--- gulpfile.js
+++ gulpfile.js
@@ -226,7 +226,7 @@ gulp.task('serve', ['styles', 'elements', 'images'], function () {
   gulp.watch(['app/**/*.html'], reload);
   gulp.watch(['app/styles/**/*.css'], ['styles', reload]);
   gulp.watch(['app/elements/**/*.css'], ['elements', reload]);
-  gulp.watch(['app/{scripts,elements}/**/*.js'], ['jshint']);
+  gulp.watch(['app/{scripts,elements}/**/{*.js,*.html}'], ['jshint']);
   gulp.watch(['app/images/**/*'], reload);
 });
 
