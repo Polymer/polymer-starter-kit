cb31ef92e5bcda3ae604bbfb52488dfca2f73fa4
Chuck Horton <chuckh@hware.com>
2015-11-20 11:21:56 -0800

    Add travis auto deploy for Github Pages

---
+++ travis-runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o pipefail
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" != "true" ]
+then
+  echo "Deploying!" && \
+  sed -i  .tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
+  rm app/scripts/app.js.tmp && \
+  bower i && \
+  gulp build-deploy-gh-page && \
+  sed -i .tmp "s/app.baseUrl = '\/polymer-starter-kit/\/\/ app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
+  rm app/scripts/app.js.tmp
+else
+  npm run lint
+fi
