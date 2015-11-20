221f7e00b006eb00e2b5b515d67281bb9564657d
Chuck Horton <chuckh@hware.com>
2015-11-20 12:20:27 -0800

    Change to use gulp build-deploy-gh-page

--- travis-runner.sh
+++ travis-runner.sh
@@ -9,14 +9,7 @@ then
   sed -i  .tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
   rm app/scripts/app.js.tmp && \
   bower i && \
-  npm run compile-all && \
-  cp CNAME dist
-  cd dist && \
-  git init && \
-  git add . && \
-  git commit -m "deploy" && \
-  git push --force --quiet "https://${GH_TOKEN}@github.com/polymerelements/polymer-starter-kit.git" master:gh-pages > /dev/null 2>&1 && \
-  cd .. && \
+  gulp build-deploy-gh-page && \
   sed -i .tmp "s/app.baseUrl = '\/polymer-starter-kit/\/\/ app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
   rm app/scripts/app.js.tmp
 else
