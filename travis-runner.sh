f6c0a6c3ef5b81caf952e3255e9fa665979f60be
Sam Saccone <samccone@gmail.com>
2015-11-24 10:11:00 -0800

    deploy: Add tokens and git config

--- travis-runner.sh
+++ travis-runner.sh
@@ -3,6 +3,8 @@ set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
+  git config --global user.email "samccone@gmail.com" && \
+  git config --global user.name "auto deployer" && \
   echo "Deploying!" && \
   sed -i.tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
   rm app/scripts/app.js.tmp && \
