de3930d595b1c22d80aaf5e8884863fa1259b31e
Chuck Horton <chuckh@hware.com>
2015-11-24 07:55:55 -0800

    Add travis-runner-sh to .travis.yml

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o pipefail
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" != "true" ]
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
   echo "Deploying!" && \
   sed -i  .tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
@@ -12,4 +12,5 @@ then
   rm app/scripts/app.js.tmp
 else
   npm run lint
+  npm test
 fi
