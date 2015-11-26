19ae44800c87b8d2c7cbe8525e500f5651f9e8fa
Chuck Horton <chuckh@hware.com>
2015-11-26 10:55:45 -0800

    Revise deploy-gh-page to use GH_TOKEN
    Note the silent: true option, it prevents publishing your token value
    in Travis logs. See http://stackoverflow.com/a/22373564/2403875.

--- gulpfile.js
+++ gulpfile.js
@@ -317,7 +317,13 @@ gulp.task('build-deploy-gh-pages', function(cb) {
 // Deploy to GitHub pages gh-pages branch
 gulp.task('deploy-gh-pages', function() {
   return gulp.src(dist('**/*'))
-    .pipe($.ghPages());
+    // Check if running task from Travis Cl, if so run using GH_TOKEN
+    // otherwise run using ghPages defaults.
+    .pipe($.if(process.env.TRAVIS === 'true', $.ghPages({
+      remoteUrl: 'https://$GH_TOKEN@github.com/proplanr/polymer-starter-kit.git',
+      silent: true,
+      branch: 'gh-pages'
+    }), $.ghPages()));
 });
 
 // Load tasks for web-component-tester
