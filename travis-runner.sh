567c89f4e544e8271dd6e355f6302f2c68bb4589
Chuck Horton <chuckh@hware.com>
2016-01-07 12:15:10 -0800

    Add Auto Deploy to Firebase

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,17 +1,49 @@
-#!/bin/bash
+#!/bin/bash -e
 set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-  git config --global user.email "samccone@gmail.com" && \
-  git config --global user.name "auto deployer" && \
-  echo "Deploying!" && \
-  sed -i.tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
-  rm app/scripts/app.js.tmp && \
-  bower i && \
-  gulp build-deploy-gh-pages && \
-  sed -i.tmp "s/app.baseUrl = '\/polymer-starter-kit/\/\/ app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
-  rm app/scripts/app.js.tmp
+  git config --global user.email "samccone@gmail.com"
+  git config --global user.name "auto deployer"
+
+  # Stamp index.html with the date and time of PSK's deploying
+  date_value=`date`
+  sed -i.tmp1 "s/This is another card./This is another card. PSK Deployed at: $date_value/" app/index.html
+
+  deploy_ghpages () {
+    # Deploying to GitHub Pages! (http://polymerelements.github.io/polymer-starter-kit)
+    sed -i.tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js
+    gulp build-deploy-gh-pages
+    # Undoing Changes to PSK for GitHub Pages
+    cp app/scripts/app.js.tmp app/scripts/app.js
+    rm app/scripts/app.js.tmp
+  }
+
+  deploy_firebase () {
+    # Deploying to Firebase! (https://polymer-starter-kit.firebaseapp.com)
+    # Making Changes to PSK for Firebase
+    sed -i.tmp 's/<!-- Chrome for Android theme color -->/<base href="\/">\'$'\n<!-- Chrome for Android theme color -->/g' app/index.html
+    sed -i.tmp "s/hashbang: true/hashbang: false/" app/elements/routing.html
+    cp app/docs/firebase.json app/firebase.json
+    # Starting Build Process for Firebase Changes
+    gulp
+    # Starting Deploy Process to Firebaseapp.com Server -- polymer-starter-kit.firebaseapp.com
+    firebase deploy --non-interactive --token "${FIREBASE_TOKEN}"
+    # Undoing Changes to PSK for Firebase
+    cp app/index.html.tmp app/index.html
+    cp app/elements/routing.html.tmp app/elements/routing.html
+    rm app/elements/routing.html.tmp
+    rm app/index.html.tmp
+    rm app/firebase.json
+  }
+
+  deploy_ghpages
+  deploy_firebase
+
+  # Revert to orginal index.html and delete temp file
+  cp app/index.html.tmp1 app/index.html
+  rm app/index.html.tmp1
+
 else
   npm run lint
   npm test
