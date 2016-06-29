e2555e11b65e98a34f81f6affe1542db53bfbc5d
Rob Dodson <robdodson@google.com>
2016-06-29 16:25:49 -0700

    First pass at polymer-build

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const babel = require('gulp-babel');
+const imagemin = require('gulp-imagemin');
+const logging = require('plylog');
+const mergeStream = require('merge-stream');
+
+const polymer = require('polymer-build');
+
+// Got problems? Try logging 'em
+// logging.setVerbose();
+
+const PolymerProject = polymer.PolymerProject;
+const fork = polymer.forkStream;
+
+let project = new PolymerProject({
+  root: process.cwd(),
+  entrypoint: 'index.html',
+  shell: 'src/psk-app.html',
+});
+
+gulp.task('default', () => {
+
+  // process source files in the project
+  let sources = project.sources()
+    .pipe(project.splitHtml())
+    // add compilers or optimizers here!
+    .pipe(gulpif('*.js', babel({
+      presets: ['es2015']
+    })))
+    .pipe(gulpif('*.{png,gif,jpg,svg}', imagemin({
+      progressive: true, interlaced: true
+    })))
+    .pipe(project.rejoinHtml());
+
+  // process dependencies
+  let dependencies = project.dependencies()
+    .pipe(project.splitHtml())
+    // add compilers or optimizers here!
+    .pipe(project.rejoinHtml());
+
+  // merge the source and dependencies streams to we can analyze the project
+  let allFiles = mergeStream(sources, dependencies)
+    .pipe(project.analyze);
+
+  // fork the stream in case downstream transformers mutate the files
+  // this fork will vulcanize the project
+  let bundled = fork(allFiles)
+    .pipe(project.bundle)
+    // write to the bundled folder
+    .pipe(gulp.dest('build/bundled'));
+
+  let unbundled = fork(allFiles)
+    // write to the unbundled folder
+    .pipe(gulp.dest('build/unbundled'));
+
+  return mergeStream(bundled, unbundled);
+});
