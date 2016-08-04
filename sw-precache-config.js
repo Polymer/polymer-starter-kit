52ad9b0462544fd6670321f2c5bf1c9a33489a95
Rob Dodson <robdodson@google.com>
2016-08-04 12:11:46 -0700

    First pass at migrating app-drawer-template

---
+++ sw-precache-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+  ],
+  navigateFallback: '/index.html'
+};
