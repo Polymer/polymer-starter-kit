57c24cec7c74c40cdd86329978af02a7cffa7e83
Chuck Horton <chuckh@hware.com>
2016-10-31 09:56:31 -0700

    Change to relative paths (#940)
    * Change to relative paths
    
    * Add comment for path in my-view404
    
    Leave absolute for 404.  @robdodson changed based on your suggestion.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   staticFileGlobs: [
-    '/index.html',
-    '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    'index.html',
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
-  navigateFallback: '/index.html'
+  navigateFallback: 'index.html'
 };
