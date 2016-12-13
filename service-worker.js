50adf3a6a9d5c349b45529479a54c6555c7adcff
Rob Dodson <robdodson@google.com>
2016-12-13 08:45:18 -0800

    Update config to include recommended. Fixup errors

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,9 @@
  * Code distributed by Google as part of the polymer project is also
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
+
+/* eslint no-console: ["error", { allow: ["info"] }] */
+
 console.info(
   'Service worker disabled for development, will be generated at build time.'
 );
