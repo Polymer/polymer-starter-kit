b820a41e7480d75920cdd3e0d1fd62640c1b4962
Abdón Rodríguez Davila <a@abdonrd.com>
2017-09-29 10:07:00 +0200

    Cache the polyfills in runtimeCaching

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -12,9 +12,21 @@
 
 module.exports = {
   staticFileGlobs: [
+    'bower_components/webcomponentsjs/webcomponents-loader.js',
+    'images/*',
     'index.html',
     'manifest.json',
-    'bower_components/webcomponentsjs/*',
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /bower_components\/webcomponentsjs\/.*.js/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          name: 'webcomponentsjs-polyfills-cache',
+        },
+      },
+    },
   ],
   navigateFallback: 'index.html',
 };
